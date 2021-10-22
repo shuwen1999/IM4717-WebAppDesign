d4b5928132e677af13a6a3c31bd096cb75174a8f
shuwen1999 <70445867+shuwen1999@users.noreply.github.com>
2021-10-22 15:54:23 +0800

    set up database

---
+++ sql/insertdata.sql
@@ -0,0 +1,46 @@
+use f32ee;
+
+insert into product_menu values
+	(1, "Chicken Chop", "main", 5),
+	(2, "Fish and Chips","main", 6),
+	(3, "Rib Eye Steak","main", 10),
+	(4, "Pork Ribs","main", 8),
+	(5, "Carbonara","main", 5),
+    (6, "Fries","sides", 2),
+    (7, "Onion Rings","sides", 2.5),
+    (8, "Salad","sides", 4),
+    (9, "Chicken Wing","sides", 3),
+    (10, "Cream of Mushroom","side", 3),
+    (11, "Coke","drinks", 1),
+    (12, "Coffee","drinks", 1),
+    (13, "Tea","drinks", 1),
+    (14, "Tiramisu","dessert", 2.5),
+    (15, "Cheesecake","dessert", 2.5),
+    (16, "Ice Cream","dessert", 2),
+    (17, "Chicken Chop + Ice Cream","promo", 6),
+    (18, "Fish and Chips + Cheesecake","promo", 8),
+    (19, "Carbonara + Soup","promo", 7);
+    
+
+
+insert into product_order values
+	(1, 3, 2, 20),
+	(2, 7, 1, 2.5),
+	(3, 1, 4, 20),
+	(4, 15, 1, 2.5),
+	(5, 19, 3, 21),
+	(6, 17, 5, 30),
+	(7, 3, 1, 10);
+
+insert into cust_details values
+    (1, "Mary", 98765432, "Hougang Blk 333 #01-02", 530333, "mary@gmail.com", 98765432, "Hougang Blk 333 #01-02", 530333, 3),
+    (2, "Tom", 97532468, "Ang Mo Kio Blk 3 #02-01", 546003, "tom2@gmail.com", 97532468, "Ang Mo Kio Blk 3 #02-01", 546003, 5),
+    (3, "Jane", 91929394, "Jurong Blk 78 #10-29", 345078, "janejane@gmail.com", 91929394, "Jurong Blk 78 #10-29", 345078, 6),
+    (4, "John", 85769403, "Tampines Blk 154 #10-30", 528154, "johnnyenglish@gmail.com", 85769403, "Tampines Blk 154 #10-30", 528154, 1);
+
+insert into payment values
+    (1, 1, "Mary", 5433345567899876, 0523, 123),
+    (2, 2, "Tom", 58674930281723857, 1222, 873),
+    (3, 3, "Jane", 2938475039494928, 1025, 952),
+    (4, 4, "John", 5433345567899876, 0523, 123);
+    
---
+++ sql/webappcreate.sql
@@ -0,0 +1,45 @@
+create table product_menu
+( productid int unsigned not null auto_increment primary key,
+  productname char(50) not null,
+  category char(50) not null,
+  price float(6,2) not null
+);
+
+create table product_order
+( orderid int unsigned not null auto_increment primary key,
+  productid int unsigned not null,
+  quantity int not null,
+  amount float(6,2)
+);
+
+create table image
+(
+    imageid int(11) not null auto_increment primary key,
+    productid int unsigned not null,
+    file_name varchar(255) COLLATE utf8_unicode_ci NOT NULL
+);
+
+create table cust_details
+(
+    custid int(11) not null auto_increment primary key,
+    custname char(50) not null,
+    custno int not null,
+    custadd char(100) not null,
+    custpostal int not null,
+    custemail char(100) not null,
+    custbillno int not null,
+    custbilladd char(100) not null,
+    custbillpostal int not null,
+    orderid int unsigned not null
+    
+);
+
+create table payment
+(
+    paymentid int(11) not null auto_increment primary key,
+    custid int unsigned not null,
+    cardname char(50) not null,
+    cardno int not null,
+    carddate int not null,
+    cardcvc int not null
+);
